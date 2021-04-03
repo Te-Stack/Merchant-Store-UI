80caa4455b4562430a2b4afdb8f665ab78bc203e
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-03 01:04:06 +0100

    phase 1 after the long break

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import "../index.css"
+
+function Home() {
+    return (
+        <div>
+            <h1>Quincy is here to takeover</h1>
+            
+        </div>
+    )
+}
+
+export default Home
