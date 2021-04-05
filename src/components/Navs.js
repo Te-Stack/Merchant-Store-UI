941d9e91f2b20722c0755dc90c3a66eaed7dbd0b
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-05 15:04:02 +0100

    error correction

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -5,21 +5,21 @@ function Navs() {
         <div>
             <nav className="navbar navbar-light bg-primary " >
             <div className ="container-fluid">
-                <a className ="navbar-brand text-white" href="#">Store made with <span>SOKO</span></a>
+                <link className ="navbar-brand text-white" to="#">Store made with <span>SOKO</span></link>
             </div>
             </nav>
 
             <nav className ="navbar navbar-expand-lg navbar-light bg-light target ">
                 <div className="container-fluid">
-                    <a className ="navbar-brand" href="#"> <i className =" text-mute fas fa-hotel icon"></i>  Target <br/> <span className="small">cham Towers, Plot 12 Nkruma Rd, Kampala, Ug </span></a>
+                    <link to="#" className ="navbar-brand" > <i className =" text-mute fas fa-hotel icon"></i>  Target <br/> <span className="small">cham Towers, Plot 12 Nkruma Rd, Kampala, Ug </span></link>
                     <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className ="navbar-toggler-icon"></span>
                     </button>
                     <div className ="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav shift ">
-                        <a className="nav-link" href="#"> <i className ="fas fa-shopping-bag icon "></i> &nbsp;&nbsp;Bag </a>
+                        <link to="#" className="nav-link" href="#"> <i className ="fas fa-shopping-bag icon "></i> &nbsp;&nbsp;Bag </link>
                         &nbsp;&nbsp;&nbsp;&nbsp;
-                        <a className ="nav-link" href="#"> <i className ="far fa-user icon "></i>&nbsp;&nbsp;  Account </a>
+                        <link className ="nav-link" href="#"> <i className ="far fa-user icon "></i>&nbsp;&nbsp;  Account </link>
                     </div>
                     </div>
                 </div>
